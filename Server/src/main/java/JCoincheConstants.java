ab2d2ab2eeb98079cd85473e5ca1253f28620fa2
Erwan Dupard <erwan.dupard@epitech.eu>
2016-11-15 11:21:16 +0100

    Adding final to const static strings

--- Server/src/main/java/JCoincheConstants.java
+++ Server/src/main/java/JCoincheConstants.java
@@ -5,20 +5,20 @@
 /**
  * Static const variables like display string and others
  */
-public class                        JCoincheConstants {
-    public static String            project_name = "JCoincheServer";
-    public static String            msg_game_started = "[>] Game Started !";
-    public static String            msg_game_stopped = "[>] Game Stopped !";
-    public static String            msg_game_waiting_queue = "[>] You Are In The Waiting Queue ..";
-    public static String            msg_welcome = "[>] Welcome !";
-    public static String            log_game_thread_status = "[>] Game Thread .. with %d clients .. Reading Queue ..";
-    public static String            log_message_found = "[>] Found Message : %s";
-    public static String            log_game_started = "[>] Game Started !";
-    public static String            log_game_stopped = "[>] Game Stopped !";
-    public static String            log_server_starting = "[>] Starting Server ..";
-    public static String            log_server_started = "[>] Server Listenning on port %d";
-    public static String            log_failed_bind = "[-] Failed to bind port %d";
-    public static String            log_client_count = "[>] Connected Clients %d";
-    public static String            log_game_process_starting = "[>] Starting Game Process ! (:";
-    public static String            log_sending_data_to_game_process = "[>] Sending [%s] To Game Process !";
+public class                                JCoincheConstants {
+    public static final String              project_name = "JCoincheServer";
+    public static final String              msg_game_started = "[>] Game Started !";
+    public static final String              msg_game_stopped = "[>] Game Stopped !";
+    public static final String              msg_game_waiting_queue = "[>] You Are In The Waiting Queue ..";
+    public static final String              msg_welcome = "[>] Welcome !";
+    public static final String              log_game_thread_status = "[>] Game Thread .. with %d clients .. Reading Queue ..";
+    public static final String              log_message_found = "[>] Found Message : %s";
+    public static final String              log_game_started = "[>] Game Started !";
+    public static final String              log_game_stopped = "[>] Game Stopped !";
+    public static final String              log_server_starting = "[>] Starting Server ..";
+    public static final String              log_server_started = "[>] Server Listenning on port %d";
+    public static final String              log_failed_bind = "[-] Failed to bind port %d";
+    public static final String              log_client_count = "[>] Connected Clients %d";
+    public static final String              log_game_process_starting = "[>] Starting Game Process ! (:";
+    public static final String              log_sending_data_to_game_process = "[>] Sending [%s] To Game Process !";
 }
