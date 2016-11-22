b1f2b5ef792cb72a702b102e89ed9b69f78a99bb
Erwan Dupard <erwan.dupard@epitech.eu>
2016-11-22 16:34:05 +0100

    adding color log to server

--- Server/src/main/java/JCoincheUtils.java
+++ Server/src/main/java/JCoincheUtils.java
@@ -14,9 +14,41 @@ public class                JCoincheUtils {
         System.out.println(str);
     }
 
-    static public void      logErr(String format, Object ...params) { System.err.println(String.format(format, params)); }
+    static public void      logInfo(String format, Object ...params) {
+        System.out.println(String.format("\033[34m" + format + "\033[0m", params));
+    }
 
-    static public void      logErr(String str) {
-        System.out.println(str);
+    static public void      logInfo(String str) {
+        System.out.println("\033[34m" + str + "\033[0m");
+    }
+
+    static public void      logSuccess(String format, Object ...params) {
+        System.out.println(String.format("\033[32m" + format + "\033[0m", params));
+    }
+
+    static public void      logSuccess(String str) {
+        System.out.println("\033[32m" + str + "\033[0m");
+    }
+
+    static public void      logWarning(String format, Object ...params) {
+        System.out.println(String.format("\033[33m" + format + "\033[0m", params));
+    }
+
+    static public void      logWarning(String str) {
+        System.out.println("\033[33m" + str + "\033[0m");
+    }
+
+    static public void      logError(String format, Object ...params) {
+        System.out.println(String.format("\033[31m" + format + "\033[0m", params));
+    }
+
+    static public void      logError(String str) {
+        System.out.println("\033[31m" + str + "\033[0m");
+    }
+
+    static public void      logStderr(String format, Object ...params) { System.err.println(String.format("\033[31m" + format + "\033[0m", params)); }
+
+    static public void      logStderr(String str) {
+        System.err.println("\033[31m" + str + "\033[0m");
     }
 }
