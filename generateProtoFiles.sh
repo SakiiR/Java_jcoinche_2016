b31285e961352374152ea111ea168567bb187cf0
Erwan Dupard <erwan.dupard@epitech.eu>
2016-11-27 17:11:25 +0100

    Fix generation with ascii art

--- generateProtoFiles.sh
+++ generateProtoFiles.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 echo "[^] Generating Google Protocol Buffers Files !"
+
+echo -ne "\033[0;33m"
+head -10 ./src/main/protobuf/JCoincheProtocol.proto
+echo -ne "\033[0;0m"
+
 protoc -I=./Server/src/main/java --java_out=./Server/src/main/java/ --proto_path=./src/main/protobuf ./src/main/protobuf/JCoincheProtocol.proto
 protoc -I=./Client/src/main/java --java_out=./Client/src/main/java/ --proto_path=./src/main/protobuf ./src/main/protobuf/JCoincheProtocol.proto
